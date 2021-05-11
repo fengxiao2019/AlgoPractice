d75a28ce8078aefecbd1fb2ebbb7e5498ee05d32
shaoyongzhe <shaoyongzhe@daixiaomi.com>
2021-05-11 09:23:40 +0800

    daily practice

--- daily/L20210507/kth.py
+++ daily/L20210507/kth.py
@@ -21,13 +21,22 @@ def kth(nums1: List[int], nums2: List[int], k: int) -> int:
 
     if mid_1 + mid_2 < k:
         if v_1 > v_2:
+            # mid_1 + mid_2 < k, 说明mid_1 + mid_2 最大为k-1，第k个元素也是排序后的第k-1个元素
+            # 如果v_1 > v_2，极端情况下v_1占着下标k-1
+            # 所以，
+            # 可以确认k不可能出现在nums2[:mid_2+1]范围内
             return kth(nums1, nums2[mid_2+1:], k - mid_2 - 1)
         else:
+            # 可以确认k不可能出现在nums1[:mid_1+1]范围内
             return kth(nums1[mid_1+1:], nums2, k - mid_1 - 1)
     else:
+        # mid_1 + mid_2 >= k ，极端情况下，mid_1 + mid_2 占着下标k
+        # 第k个元素在下标k的左侧
         if v_1 > v_2:
+            # 在上述极端情况下，v_1 占着下标k，所以k不可能出现在nums1[mid_1:]范围内
             return kth(nums1[:mid_1], nums2, k)
         else:
+            # 同理，在极端情况下，v_2占着下标k，所以k不可能出现在nums2[mid_2:]范围内
             return kth(nums1, nums2[:mid_2], k)
 
 """
@@ -40,4 +49,4 @@ import unittest
 
 class TestKth(unittest.TestCase):
     def test_kth(self):
-        self.assertEqual(kth([1, 4, 7, 9], [2, 6, 10], 1), 2)
+        self.assertEqual(kth([3, 6, 8, 9], [2, 4, 5, 8], 4), 6)
