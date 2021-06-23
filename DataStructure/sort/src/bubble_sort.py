15e25b2a1eec1f4bfe7558852c554c516724080f
shaoyongzhe <shaoyongzhe@daixiaomi.com>
2021-06-23 08:27:32 +0800

    notes

--- DataStructure/sort/src/bubble_sort.py
+++ DataStructure/sort/src/bubble_sort.py
@@ -4,13 +4,8 @@
 class BubbleSort(object):
     def __call__(self, nums):
         self._helper(nums)
-
     def _helper(self, nums):
-        i = len(nums) - 1
-        while i >= 0:
-            j = 1
-            while j <= i:
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(1, i+1):
                 if nums[j - 1] > nums[j]:
                     nums[j], nums[j - 1] = nums[j - 1], nums[j]
-                j += 1
-            i -= 1
